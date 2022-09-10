880cc41a7c84224579a0789050a6f83beaa2f144
Mitchell07 <michaelokpara99@gmail.com>
2022-09-10 04:52:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  int i;
+  /* your code goes there */
+  for    (i = 0;    i < 10;    i++)
+    {
+      printf("%d", i);
+    }
+  printf("\n");
+  return (0);
+}

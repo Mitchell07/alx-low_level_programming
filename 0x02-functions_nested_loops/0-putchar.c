b2a3cb03bbeb6109be314c547364adf0fb6d7a7e
Mitchell07 <michaelokpara99@gmail.com>
2022-09-17 00:30:19 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,14 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	
-	char name[20] = "_putchar";
+  char name[20] = "_putchar";
 
-	printf("%s\n", name);
-	
-	return (0);
+  printf("%s\n", name);
+
+  return (0);
 }
